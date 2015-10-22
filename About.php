c24d3bda20557698a154b09ca1cbf2ba67c75511
Charlie Steenhagen <gcs@iastate.edu>
2015-10-21 19:26:03 -0500

    no message

---
+++ About.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>About | Steenhagen</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href="Style.css" rel="stylesheet">
+</head>
+<body>
+
+<?php include('header.php');?>
+
+<div class="ID-Me">
+    <img src="6220d255154fad0c911a3cb4c0072031.jpg" >
+    <h1>Gregory Steenhagen</h1>
+    <hr style="width: 300px; margin-top: -8px; margin-bottom: -8px;">
+    <p>Software Engineering Major at Iowa State University<br>Vice President and Treasurer of the ISU Web Development Club</p>
+
+
+</div>
+<div class="Text">
+    <h1>About Me</h1>
+    <p>My name is Gregory Steenhagen and I am a software engineering major at Iowa State University. I grew up just 15 minutes
+    south of Ames in Roland. I graduated from Roland-Story high school where I participated in football, basketball, track, and baseball
+    all four years. I was in band, chorus, jazz band, and the musicals all four years as well. Being so busy with extra curricular activities,
+    I didn't have time to realize I was a computer nerd. I was always interested in computers, but it took until college for me to
+    figure out what I wanted to do with me life. I have always been an ISU fan and I always knew I wanted to go there so that choice was easy for me.
+    </p>
+    <p>I started college as a Mechanical Engineering major and quickly decided to switch to software, after I realized computers
+    were what I truly enjoyed working with. After switching, a friend and I started the Web Development Club at ISU of which I am
+    the Vice President and Treasurer. </p>
+</div>
+<div class="Languages">
+    <div class="container">
+        <h1>Languages</h1>
+        <hr>
+        <div class="row">
+            <div class="col-xs-2"><img src="Java.png"></div>
+            <div class="col-xs-2"><img src="python_logo.png"></div>
+            <div class="col-xs-2"><img src="C++-unofficial.sh-600x600.png"></div>
+            <div class="col-xs-2"><img src="C.png"></div>
+            <div class="col-xs-2"><img src="HTML5_Logo_256.png"></div>
+            <div class="col-xs-2"><img src="css3_logo.png"></div>
+        </div>
+    </div>
+</div>
+<?php include('footer.php');?>
+
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href="Style.css" rel="stylesheet">
+</head>
+
+<body>
+<footer>
+    <div id="wrapper">
+        <div id="footer">
+            <div class="footerFloat">
+                <h4><a href="index.php" style="text-decoration:underline;">Home</a></h4>
+                <p>Visit my home page for an overview of myself, my website and more!</p>
+            </div>
+            <div class="footerFloat">
+                <h4><a href="Portfolio.php" style="text-decoration:underline;">Portfolio</a></h4>
+                <p><a href="Websites.php">Websites</a></p>
+                <p><a href="Software.php">Software</a></p>
+                <p><a href="Hardware.php">Hardware</a></p>
+            </div>
+            <div class="footerFloat">
+                <h4 style="text-decoration:underline;"><a href="About.php">About</a></h4>
+                <p>Visit this page to learn more about my skills and accomplishments.</p>
+            </div>
+            <div class="footerFloat">
+                <h4><a href="Contact.php" style="text-decoration:underline;">Contact</a></h4>
+                <p>For any questions regarding my website, don't hesitate to contact me!</p>
+            </div>
+        </div>
+    </div>
+
+</footer>
+</body>
+</html>
