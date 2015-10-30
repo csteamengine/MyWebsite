9032c7c0def454a76a63630ec90aaf8740ba1af1
csteamengine <gcs@iastate.edu>
2015-10-30 15:13:03 -0500

    no message

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Home | Stonestreet</title>
+    <title>Home | StoneStreet</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,27 +14,116 @@
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="js/jquery-1.9.1.min.js"></script>
+    <script src="js/modernizr.js"></script>
+    <script>
+        $(document).ready(function(){
+            if (Modernizr.touch) {
+                // show the close overlay button
+                $(".close-overlay").removeClass("hidden");
+                // handle the adding of hover class when clicked
+                $(".img").click(function(e){
+                    if (!$(this).hasClass("hover")) {
+                        $(this).addClass("hover");
+                    }
+                });
+                // handle the closing of the overlay
+                $(".close-overlay").click(function(e){
+                    e.preventDefault();
+                    e.stopPropagation();
+                    if ($(this).closest(".img").hasClass("hover")) {
+                        $(this).closest(".img").removeClass("hover");
+                    }
+                });
+            } else {
+                // handle the mouseenter functionality
+                $(".img").mouseenter(function(){
+                    $(this).addClass("hover");
+                })
+                    // handle the mouseleave functionality
+                    .mouseleave(function(){
+                        $(this).removeClass("hover");
+                    });
+            }
+        });
+    </script>
     <![endif]-->
     <link href="Style.css" rel="stylesheet">
 </head>
 <?php include('header.php');?>
-<div class="wrapper">
-    <div class="Text">
+<div class="Text">
+    <h1>A little about me</h1>
+    <p>I am a Software engineering major at Iowa State University and I'm currently in my Junior year. I am the Vice President,
+        Treasurer, and a founding member of the ISU Web Development Club. As a kid I always had an interest in computers, and
+        Iowa State gave me the opportunity to make them my career. </p>
+    <hr>
+    <h1>Most Recent Projects</h1>
+</div>
+<div id="effect-5" class="effects clearfix">
+    <div class="img">
 
-        <p>Welcome to my website. I am setting up some stuff right now.</p>
-
-        <p>Stonestreet Software</p>
-        <p>
-            Bacon ipsum dolor amet doner bacon pig boudin picanha. Landjaeger turkey tri-tip boudin, swine flank short ribs tail meatloaf pork chop
-            rump kevin pastrami meatball. Sausage corned beef filet mignon hamburger venison ball tip flank ham, pastrami meatball short loin prosciutto kielbasa.
-            Boudin landjaeger flank, shankle beef salami cow brisket filet mignon t-bone meatball corned beef bacon pork chop tenderloin.
-
-            Boudin ham hock shoulder, hamburger pork salami ribeye venison pork loin kielbasa porchetta turkey. Short loin capicola tri-tip kielbasa shoulder,
-            turducken cow biltong meatloaf bacon tenderloin beef kevin. Boudin swine pastrami kielbasa pig short ribs alcatra spare ribs pork loin beef shoulder.
-            Turkey alcatra biltong ribeye meatloaf. Leberkas strip steak ground round, chuck shoulder pork loin rump ham hock landjaeger bacon pork chop short ribs beef
-            ribs biltong. Kevin bacon jerky kielbasa filet mignon,
-            tri-tip landjaeger brisket pork loin spare ribs.
-        </p>
+        <img src="Images/apple-desk-office-technology.jpg" alt="">
+        <div class="overlay">
+            <p>Project Name</p>
+            <a href="Portfolio.php" class="expand">+</a>
+            <a class="close-overlay hidden">x</a>
+        </div>
+    </div>
+    <div class="img">
+        <img src="Images/apple-desk-office-technology.jpg" alt="">
+        <div class="overlay">
+            <p>Project Name</p>
+            <a href="Portfolio.php" class="expand">+</a>
+            <a class="close-overlay hidden">x</a>
+        </div>
+    </div>
+    <div class="img">
+        <img src="Images/apple-desk-office-technology.jpg" alt="">
+        <div class="overlay">
+            <p>Project Name</p>
+            <a href="Portfolio.php" class="expand">+</a>
+            <a class="close-overlay hidden">x</a>
+        </div>
     </div>
 </div>
+
+<div class="Text">
+    <hr>
+    <h1>Most Recent Accomplishment</h1>
+</div>
+
+<div id="effect-5" class="effects1 clearfix">
+    <div class="img">
+        <img src="Images/apple-desk-office-technology.jpg" alt="">
+        <div class="overlay">
+            <p>Project Name</p>
+            <a href="Portfolio.php" class="expand">+</a>
+            <a class="close-overlay hidden">x</a>
+        </div>
+    </div>
+</div>
+
+<div class="Text">
+    <p>Recently, a friend and I won the Rockwell Collins award at HackISU, an event where computer nerds gather
+        to develop software or hardware projects. We developed a self balancing robot with lots of wires and circuit boards.
+        We spent less than $10 on the robot and won over $500 in prizes.
+    </p>
+    <hr>
+    <h1>A Brief Look at My Life</h1>
+</div>
+<p id="img_cont">
+    <a href="#"><img src="Images/apple-laptop-notebook-notes.jpg" /></a>
+    <a href="#"><img src="Images/apple-laptop-notebook-notes.jpg" /></a>
+    <a href="#"><img src="Images/apple-laptop-notebook-notes.jpg" /></a>
+    <a href="#"><img src="Images/apple-laptop-notebook-notes.jpg" /></a>
+    <a href="#"><img src="Images/apple-laptop-notebook-notes.jpg" /></a>
+    <a href="#"><img src="Images/apple-laptop-notebook-notes.jpg" /></a>
+</p>
+<div class="Text">
+    <hr>
+</div>
+<div class="HireMe">
+    <p>Want to hire me today? &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<button class="btn">Contact Me</button></p>
+</div>
 <?php include('footer.php');?>
+
