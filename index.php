a46915161b88774f4c664969de6ede6978383c35
csteamengine <gcs@iastate.edu>
2015-10-30 15:48:31 -0500

    no message

--- index.php
+++ index.php
@@ -14,39 +14,7 @@
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <script src="js/jquery-1.9.1.min.js"></script>
-    <script src="js/modernizr.js"></script>
-    <script>
-        $(document).ready(function(){
-            if (Modernizr.touch) {
-                // show the close overlay button
-                $(".close-overlay").removeClass("hidden");
-                // handle the adding of hover class when clicked
-                $(".img").click(function(e){
-                    if (!$(this).hasClass("hover")) {
-                        $(this).addClass("hover");
-                    }
-                });
-                // handle the closing of the overlay
-                $(".close-overlay").click(function(e){
-                    e.preventDefault();
-                    e.stopPropagation();
-                    if ($(this).closest(".img").hasClass("hover")) {
-                        $(this).closest(".img").removeClass("hover");
-                    }
-                });
-            } else {
-                // handle the mouseenter functionality
-                $(".img").mouseenter(function(){
-                    $(this).addClass("hover");
-                })
-                    // handle the mouseleave functionality
-                    .mouseleave(function(){
-                        $(this).removeClass("hover");
-                    });
-            }
-        });
-    </script>
+
     <![endif]-->
     <link href="Style.css" rel="stylesheet">
 </head>
@@ -125,5 +93,38 @@
 <div class="HireMe">
     <p>Want to hire me today? &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<button class="btn">Contact Me</button></p>
 </div>
+<script src="js/jquery-1.9.1.min.js"></script>
+<script src="js/modernizr.js"></script>
+<script>
+    $(document).ready(function(){
+        if (Modernizr.touch) {
+            // show the close overlay button
+            $(".close-overlay").removeClass("hidden");
+            // handle the adding of hover class when clicked
+            $(".img").click(function(e){
+                if (!$(this).hasClass("hover")) {
+                    $(this).addClass("hover");
+                }
+            });
+            // handle the closing of the overlay
+            $(".close-overlay").click(function(e){
+                e.preventDefault();
+                e.stopPropagation();
+                if ($(this).closest(".img").hasClass("hover")) {
+                    $(this).closest(".img").removeClass("hover");
+                }
+            });
+        } else {
+            // handle the mouseenter functionality
+            $(".img").mouseenter(function(){
+                $(this).addClass("hover");
+            })
+                // handle the mouseleave functionality
+                .mouseleave(function(){
+                    $(this).removeClass("hover");
+                });
+        }
+    });
+</script>
 <?php include('footer.php');?>
 
