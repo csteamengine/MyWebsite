d0c0c1cf2157d35e7d49221a8a31e69a46e61807
csteamengine <gcs@iastate.edu>
2015-11-12 08:40:50 -0600

    Got it perfect. Header has parallax effect. Otherwise it is just static, which allows the page to scroll faster. Looks really Good.

--- header.php
+++ header.php
@@ -1,38 +1,39 @@
 
 <body>
+<section id="home" data-type="background" data-speed="10" class="pages">
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="cover-background" >
+            <div class="cover-image">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
 
-<nav class="navbar navbar-default" role="navigation">
-    <div class="cover-background" >
-        <div class="cover-image">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+                <div class="collapse navbar-collapse  navbar-ex1-collapse navbar-left">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php">Home</a></li>
+                        <li class="dropdown">
+                            <a href="Portfolio.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Portfolio<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="Websites.php">Websites</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="Software.php">Software</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="Hardware.php">Hardware</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="About.php">About</a></li>
+                        <li><a href="Contact.php">Contact</a></li>
+                    </ul>
+                </div>
             </div>
-
-            <div class="collapse navbar-collapse  navbar-ex1-collapse navbar-left">
-                <ul class="nav navbar-nav">
-                    <li><a href="index.php">Home</a></li>
-                    <li class="dropdown">
-                        <a href="Portfolio.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Portfolio<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="Websites.php">Websites</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="Software.php">Software</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="Hardware.php">Hardware</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="About.php">About</a></li>
-                    <li><a href="Contact.php">Contact</a></li>
-                </ul>
+            <div class="logo">
+                <img src="Images/OneSLogo.png">
             </div>
         </div>
-        <div class="logo">
-            <img src="Images/OneSLogo.png">
-        </div>
-    </div>
-</nav>
+    </nav>
+</section>
